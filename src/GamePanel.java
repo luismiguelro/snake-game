94bef3cff2a68ed85ddb64d54289b88739d2f41c
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-01-16 18:58:00 -0500

    snakegame, gameframe and game panel classes; methods to be used in gamepanel class

---
+++ src/GamePanel.java
@@ -0,0 +1,56 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import  java.util.Random;
+import  javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+    GamePanel(){
+    }
+
+    /* Methods to be used*/
+
+    //start game
+    public void startGame(){
+
+    }
+
+    // paint component
+    public void paintComponent(Graphics g){
+
+    }
+
+    // draw
+    public void draw(Graphics g){
+
+    }
+
+    //move
+    public void move(){
+
+    }
+
+    // points
+    public void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public class myKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
+}
