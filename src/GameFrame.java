a9ab90fd83a359f0d87aba314580fac1cceae383
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-01-17 13:49:34 -0500

    draw lines, blocks for the game and movements (left, right, up and down)

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,7 +4,7 @@ public class GameFrame extends JFrame {
     GameFrame(){
 
         this.add(new GamePanel());
-        this.setTitle("Snake");
+        this.setTitle("Snake Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -36,7 +36,7 @@ public class GamePanel extends JPanel implements ActionListener {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
-        this.addKeyListener(new MyKeyAdapter());
+        this.addKeyListener(new myKeyAdapter());
         startGame();
     }
 
@@ -44,25 +44,57 @@ public class GamePanel extends JPanel implements ActionListener {
 
     //start game
     public void startGame(){
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
     }
 
     // paint component
     public void paintComponent(Graphics g){
-
+        super.paintComponent(g);
+        draw(g);
     }
 
     // draw
     public void draw(Graphics g){
-
+        //form a kind of grid
+        for (int i = 0; i <SCREEN_HEIGHT/UNIT_SIZE ; i++) {
+            //draw the lines
+            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
     }
 
+    //Generate apples randomly
     public void newApple(){
-        
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleX = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+
     }
     //move
     public void move(){
+        for (int i = bodyParts; i >0 ; i--) {
+            x[i]= x[i-1];
+            y[i]= y[i-1];
+        }
 
+        switch (direction){
+            case 'U':
+                y[0]= y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0]= y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0]= x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0]= x[0] + UNIT_SIZE;
+                break;
+        }
     }
 
     // points
