75a8fafcd33eb3cb2f63fb12618673591a2c6bf3
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-01-16 19:24:30 -0500

    variables we need for the game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,6 +6,31 @@ import  java.util.Random;
 import  javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH=600;
+    static final int SCREEN_HEIGHT=600;
+
+    /*size of game objects*/
+    static final int UNIT_SIZE=25;
+
+    /*elements that fit on the screen*/
+    static final int GAME_UNITS= (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+
+    /*timer*/
+    static final int DELAY=75;
+
+    /*matrices that will have the coordinates where the snake will move through*/
+    final int x[]= new int[GAME_UNITS]; //body parts
+    final int y[] = new int[GAME_UNITS]; // has all the coordinates
+
+    int bodyParts =6;
+    int applesEaten;
+    int appleX; // random apples
+    int appleY; // positioning of the apples
+    char direction = 'R'; // start of movement
+    boolean running = false;
+    Timer timer;
+    Random random;
     GamePanel(){
     }
 
